98fa7215e9cda6c1a981d604cbd6fedbc600f0d1
BHUMIHAR SAURABH (KATIL) <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-06-03 20:50:57 +0530

    tmc

--- TmcStringSessionBot/basic.py
+++ TmcStringSessionBot/basic.py
@@ -15,25 +15,6 @@ async def start(bot: Client, msg: Message):
     await bot.send_message(
         msg.chat.id,
         Data.START.format(msg.from_user.mention, mention),
-        reply_markup=InlineKeyboardMarkup(Data.buttons)
-    )
-
-
-# Help Message
-@Client.on_message(filter("help"))
-async def _help(bot: Client, msg: Message):
-    await bot.send_message(
-        msg.chat.id, Data.HELP,
-        reply_markup=InlineKeyboardMarkup(Data.home_buttons)
-    )
-
-
-# About Message
-@Client.on_message(filter("about"))
-async def about(bot: Client, msg: Message):
-    await bot.send_message(
-        msg.chat.id,
-        Data.ABOUT,
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
+        reply_markup=InlineKeyboardMarkup(Data.buttons)
     )
